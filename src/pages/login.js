8a37cef8ad7ad0d203a282b0b05f23e3cb45a4bc
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-08-05 14:17:43 -0700

    testing git

--- src/pages/login.js
+++ src/pages/login.js
@@ -23,7 +23,7 @@ const Login = () => {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <h1>Login</h1>
+      <h1>Login Page</h1>
     </div>
   );
 };
