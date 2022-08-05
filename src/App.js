34c659701e9efb9eb3731e8677470aba984e6403
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-08-05 14:02:36 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 
-const Login = lazy(() => import('./pages/login'));
+const Login = lazy(() => import('./pages/Login'));
 
 function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading... </p>}>
         <Routes>
-          <Route path="/login" component={Login} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
         </Routes>
       </Suspense>
     </Router>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
-const login = () => {
-  return <div>login</div>;
+const Login = () => {
+  // const history = useHistory();
+  const history = useNavigate();
+  // history('/home');
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <h1>Login</h1>
+    </div>
+  );
 };
 
-export default login;
+export default Login;
